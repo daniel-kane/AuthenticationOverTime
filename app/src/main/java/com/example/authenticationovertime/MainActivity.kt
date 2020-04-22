e054bff86561437e40c909f4af52d496363ca304
daniel-kane <daniel.kane7@gmail.com>
2020-04-21 21:55:26 -0400

    added basic location services functionality

--- app/src/main/java/com/example/authenticationovertime/MainActivity.kt
+++ app/src/main/java/com/example/authenticationovertime/MainActivity.kt
@@ -1,12 +1,143 @@
+/*
+This code follows a tutorial by Ferdousur Rahman Sarker on getting location in kotlin
+https://www.androdocs.com/kotlin/getting-current-location-latitude-longitude-in-android-using-kotlin.html
+
+They provide some useful functions for checking and obtaining permission to use the location,
+and then show how to use the Fused Location Prodiver API to get the location
+
+It is not exactly what we need to do, but it is a step in the right direction, and we can
+reference this code for our location accessing needs.
+
+If you're using an emulator, you'll get the location of the google headquarters. Extra steps
+are required for the emulator, but I'm not sure how to do that right now.
+
+ */
+
 package com.example.authenticationovertime
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Any random number
+    val PERMISSION_ID = 42
+
+    //use the actual Fused Location Provider API to get users current position
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        //Call this in a loop/ at an interval. Should figure out how to get
+        //location when the app is closed/in background
+        getLastLocation()
+    }
+
+    //Checks if GPS or newtork providor is enabled for location manager
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    //Checks if the permission was granted by user to use location
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+
+    //Requests permission from the user
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Granted. Start getting the location information
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+
+        //If the permissions were given and location is enabled
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        findViewById<TextView>(R.id.latTextView).text = location.latitude.toString()
+                        findViewById<TextView>(R.id.lonTextView).text = location.longitude.toString()
+                    }
+                }
+            } else {
+
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            //Requests user permission
+            requestPermissions()
+        }
+    }
+
+    //"in rare cases, the location can be null, so we do this, which record the location info in runtime
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+
+        //TO get location every 5-10 seconds, change the values above to 10000 and 5000
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        //When we get an update, do the mLocationCallback code and update textviews
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            findViewById<TextView>(R.id.latTextView).text = mLastLocation.latitude.toString()
+            findViewById<TextView>(R.id.lonTextView).text = mLastLocation.longitude.toString()
+        }
     }
 }
